74435fce5fcb8d4378b88f6eb6d7990729547da2
VincentMarie <21500338etucaen@gmail.com>
2016-05-06 19:06:53 +0200

    test

--- app/controllers/Disques.php
+++ app/controllers/Disques.php
@@ -8,18 +8,17 @@ class Disques extends \_DefaultController {
 	}
 
 	public function form($id=NULL){
-		+		$disque=$this->getInstance($id);
-		+		$disabled="";
-		+		$this->loadView("views/Disques/form.html",array("disque"=>$disque,"disabled"=>$disabled));
-		+	}
-+
-+	/* (non-PHPdoc)
-+	 * @see _DefaultController::setValuesToObject()
-+	 */
-+
-+	protected function setValuesToObject(&$object) {
-	+		parent::setValuesToObject($object);
-	+		$object->setUtilisateur(Auth::getUser());
-	+	}
+		$disque=$this->getInstance($id);
+		$disabled="";
+		$this->loadView("views/Disques/form.html",array("disque"=>$disque,"disabled"=>$disabled));
+		}
+
+	/* (non-PHPdoc)
+	 * @see _DefaultController::setValuesToObject()
+	 */
+
+	protected function setValuesToObject(&$object) {
+		parent::setValuesToObject($object);
+		$object->setUtilisateur(Auth::getUser());
+	}
 }
-?>
